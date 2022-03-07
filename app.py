3935acb1d5d5076716284bea3f3dbe7f80f8e2fb
kim-ju-ho <kim1jo2ho3@naver.com>
2022-03-07 17:32:21 +0900

    bookMarker 초기세팅

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login/login.html')
+
+
+@app.route('/rgstr')
+def rgstr():
+    return render_template('login/rgstr.html')
+
+
+@app.route('/detail')
+def detail():
+    return render_template('detail.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
