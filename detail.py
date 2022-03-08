98a0cc82056714f4f818c47c434e8bcfa0ee2cc6
kim-ju-ho <kim1jo2ho3@naver.com>
2022-03-08 17:27:01 +0900

    detail comment write

--- detail.py
+++ detail.py
@@ -1,8 +1,38 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, request, jsonify
+from pymongo import MongoClient
+import certifi
 detail_blueprint = Blueprint("detail", __name__, url_prefix="/detail")
+
+ca = certifi.where()
+client = MongoClient('mongodb+srv://test:sparta@cluster0.lafvq.mongodb.net/Cluster0?retryWrites=true&w=majority',
+                     tlsCAFile=ca)
+mongo_db = client.bookmaker
 
 
 @detail_blueprint.route("/detail")
 def detail():
     return render_template("detail/detail.html", str="detail 나와요?")
+
+
+# 코멘트 작성
+@detail_blueprint.route("/write_paragraph", methods=["POST"])
+def write_paragraph():
+    if request.method == 'POST':
+        book_num = request.form.get('bookNum')
+        para_num = request.form.get('paraNum')
+        user_id = request.form.get('userId')
+        para_content = request.form.get('paraContent')
+        write_date = request.form.get('writeDate')
+
+        paragrpah = {
+            'bookNum': book_num,
+            'paraNum': para_num,
+            'userId': user_id,
+            'paraContent': para_content,
+            'writeDate': write_date
+        }
+        mongo_db.paragraph.insert_one(paragrpah)
+        print(paragrpah)
+    else:
+        return jsonify({'msg': '저장실패!'})
+    return jsonify({'msg': '저장성공!'})
--- static/js/detail.js
+++ static/js/detail.js
@@ -3,8 +3,40 @@ $(function(){
 
 
 });
-function pareShare(){
-         console.log("나오냐?");
+// 현재 날짜 가져오기
+function todayDate(){
+    let today = new Date();
+    let year = today.getFullYear();     // 년도
+    let month = today.getMonth() + 1;   // 월
+    let date = today.getDate();         // 날짜
+
+    return year + '-' + month + '-' + date;
+
+}
+function paraShare(){
+    let bookNum = "b"+1;
+    let paraNum = "p"+1;
+    let userId  = "testUser01";
+    let paraContent = "책설명 입니당";
+    let writeDate = todayDate();
+    console.log("ASd");
+       $.ajax({
+            type: 'POST',
+            url: '/detail/write_paragraph',
+            data: {
+                'bookNum': bookNum,
+                'paraNum': paraNum,
+                'userId': userId,
+                'paraContent': paraContent,
+                'writeDate': writeDate
+            },
+            success: function (response) {
+                console.log(response)
+
+            },error: function (error){
+                console.log(error)
+           }
+        })
     }
 
 
